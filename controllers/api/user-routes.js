8c26b9e87f27942ff41aad7ee09593fbbcf2b42e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-21 19:59:24 -0500

    user and thoughts routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,18 @@
+const {User} = require("../../models");
+const router = require("express").Router();
+
+//get all users
+router.get("/", (req, res) => {
+    User.find({})
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  });
