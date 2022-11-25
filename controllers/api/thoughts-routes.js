08f6a8514cc971e3219becb53468c27b3d4977ea
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-11-25 02:42:48 -0500

    working in thought routes

--- controllers/api/thoughts-routes.js
+++ controllers/api/thoughts-routes.js
@@ -12,3 +12,52 @@ Thought.get("/",(req, res)=>{
       res.sendStatus(400);
     });
 });
+
+// get thought 
+router.get("/:id", (req, res) => {
+  Thought.findOne({_id: req.params.id })
+  .select("-__v")
+  .then((dbThoughtData) => res.json(dbThoughtData))
+  .catch((err) => {
+    console.log(err);
+    res.sendStatus(400);
+  });
+});
+
+// add a thought
+router.post("/", (req, res) => {
+  Thought.create(req.body)
+  .then((dbThoughtData) => {
+    return User.findOneAndUpdate(
+      {_id: req.body.userId },
+      {$push: {thoughts: dbThoughtData._id} },
+      { new: true}
+    );
+  })
+  .then((dbUserData) => {
+    id(!dbUserData) {
+      res.status(404).json({message: "invaild user id."});
+      return;
+    }
+    res.jsin(dbUserData);
+  })
+  .catch((err) => res.json(err));
+});
+
+//update thougt
+router.put("/:id", ({params, body}, res) => {
+  Thought.findOneAndUpdate({ _id: params.id}, body, {
+    new: true,
+    runValidators: true,
+  })
+  .then((dbThoughtData) => {
+    if(!dbThoughtData) {
+      res.status(404).json({ message: " invaild user id."});
+      return;
+    }
+    res.json(dbThoughtData);
+  })
+  .catch((err) => res.json(err));
+});
+
+/
